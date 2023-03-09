511dd7f661006534c0a0b92291d527623256d37a
Blaž Erzar <blaz.erzar@gmail.com>
2023-03-09 21:07:51 +0100

    Fixed sink name for audio

--- i3/scripts/toggle_sound_device.sh
+++ i3/scripts/toggle_sound_device.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-SINK=alsa_output.pci-0000_00_1f.3.analog-stereo
+SINK=alsa_output.pci-0000_00_1f.3.3.analog-stereo
 SPEAKERS=analog-output-lineout
 HEADPHONES=analog-output-headphones
 
